d18228a13f0e7716b105df6a36b9fe6c52d4c059
shafirpl <shafi.rpl@gmail.com>
2020-05-02 16:45:38 -0700

    Initial commit

---
+++ Android/app/src/main/java/com/example/companionandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.companionandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+    
+}
