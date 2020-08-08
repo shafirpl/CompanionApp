6ac4c073d350dea630bdb5a3029e24cb49dc77de
shafirpl <shafi.rpl@gmail.com>
2020-08-08 06:29:45 -0700

    ios app started

---
+++ iOS/CompanionApp/CompanionApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CompanionApp
+//
+//  Created by shafi on 2020-08-08.
+//  Copyright © 2020 shafirpl. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS/CompanionApp/CompanionApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CompanionApp
+//
+//  Created by shafi on 2020-08-08.
+//  Copyright © 2020 shafirpl. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
