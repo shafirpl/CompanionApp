a3e0d47e6d4e8240a5d809782d619c7e2ada9da4
shafirpl <shafi.rpl@gmail.com>
2020-08-08 19:31:03 -0700

    Starting on notes on ios

--- iOS/CompanionApp/CompanionApp/FirstViewController.swift
+++ iOS/CompanionApp/CompanionApp/FirstViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
---
+++ iOS/CompanionApp/Pods/Target Support Files/Pods-CompanionApp-CompanionAppUITests/Pods-CompanionApp-CompanionAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CompanionApp_CompanionAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CompanionApp_CompanionAppUITests
+@end
